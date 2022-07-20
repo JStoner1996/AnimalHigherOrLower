b76e14ee95f2c0e447f746d73111eb5bbc9c9b3c
JStoner1996 <jakestoner123@live.com>
2022-07-20 14:54:58 +0100

    added functionality

--- reanimal/src/components/RandomAnimal.js
+++ reanimal/src/components/RandomAnimal.js
@@ -6,6 +6,7 @@ export const names = [];
 export default function RandomAnimal() {
   const [animals, setAnimal] = useState([]);
   const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   const checkGreaterTop = () => {
     if (lifespans[0] >= lifespans[1]) {
@@ -28,6 +29,9 @@ export default function RandomAnimal() {
       `You Win! ${names[0]}s live to ${lifespans[0]}, ${names[1]}s live to ${lifespans[1]}!`
     );
     setScore(score + 1);
+    if (score > highScore) {
+      setHighScore(score);
+    }
     fetchAnimals();
   };
 
@@ -35,7 +39,11 @@ export default function RandomAnimal() {
     alert(
       `You Lose! ${names[0]}s live to ${lifespans[0]}, ${names[1]}s live to ${lifespans[1]}. Your score was ${score}!`
     );
+
     setScore(0);
+    if (score > highScore) {
+      setHighScore(score);
+    }
     fetchAnimals();
   };
 
@@ -56,62 +64,67 @@ export default function RandomAnimal() {
 
   if (animals) {
     return (
-      <>
-        <div class="container-fluid">
-          <p>Score: {score}</p>
-          {animals.map((animal, id) => (
-            <>
-              <div class="row">
-                <div class="col-md-12"></div>
-              </div>
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="card">
-                    <h5 class="card-header">{animal.name}</h5>
-                    <div class="card-body">
-                      <ul class="card-text">
-                        <li>Type: {animal.animal_type}</li>
-                        <li>Active Time: {animal.active_time}</li>
-                        <li>
-                          {" "}
-                          Length Range: {animal.length_min}-{animal.length_max}
-                          feet{" "}
-                        </li>
-                        <li>
-                          Weight Range: {animal.weight_min}-{animal.weight_max}
-                          lbs
-                        </li>
-                        {/* <li>Lifespan: {lifespan[id]} Years</li> */}
-                        <li>Habitat: {animal.habitat}</li>
-                        <li>Diet: {animal.diet}</li>
-                        <li>Geological Range: {animal.geo_range}</li>
-                      </ul>
-                    </div>
+      <div className="container-fluid">
+        <p>
+          Score: {score} High Score: {highScore}
+        </p>
+        {animals.map((animal, id) => (
+          <div key={id}>
+            <div className="row m-2">
+              <div className="col-md-6">
+                <div className="card">
+                  <h5 className="card-header">{animal.name}</h5>
+                  <div className="card-body">
+                    <ul className="card-text">
+                      <li>Type: {animal.animal_type}</li>
+                      <li>Active Time: {animal.active_time}</li>
+                      <li>
+                        {" "}
+                        Length Range: {animal.length_min}-{animal.length_max}
+                        feet{" "}
+                      </li>
+                      <li>
+                        Weight Range: {animal.weight_min}-{animal.weight_max}
+                        lbs
+                      </li>
+                      {/* <li>Lifespan: {lifespan[id]} Years</li> */}
+                      <li>Habitat: {animal.habitat}</li>
+                      <li>Diet: {animal.diet}</li>
+                      <li>Geological Range: {animal.geo_range}</li>
+                    </ul>
                   </div>
                 </div>
-
-                <div class="col-md-4">
-
-
+              </div>
+              <div className="col-md-6">
                 <img
                   alt={animal.name}
                   src={animal.image_link}
-                  class="rounded align-self-center"
-                  />
-                  </div>
-                <div class="col-md-4">
-                  <button
-                    type="button"
-                    class="btn btn-lg btn-block btn-primary"
-                  >
-                    Button
-                  </button>
-                </div>
+                  className="rounded mx-auto"
+                />
               </div>
-            </>
-          ))}
+            </div>
+          </div>
+        ))}
+        <div className="row">
+          <div className="col-md-6">
+            <button
+              type="button"
+              className="btn btn-success btn-lg btn-block m-2"
+              onClick={checkGreaterTop}
+            >
+              Top
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-success btn-lg btn-block m-2"
+              onClick={checkGreaterBottom}
+            >
+              Bottom
+            </button>
+          </div>
         </div>
-      </>
+      </div>
     );
   }
 }
