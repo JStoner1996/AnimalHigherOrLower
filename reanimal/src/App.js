67a21d1a462653e99ac9e1c9a454b43ee5cecbdf
JStoner1996 <jakestoner123@live.com>
2022-07-21 09:46:59 +0100

    added localstorage saves

--- reanimal/src/App.js
+++ reanimal/src/App.js
@@ -1,17 +1,20 @@
-import React from "react";
+
 import "./App.css";
 import RandomAnimal from "./components/RandomAnimal";
 import Popup from "./components/Popup";
 
 function App() {
+
   return (
     <>
       <h1>Welcome to Re-Animal </h1>
-
+      <p className="text-center">Click the name of the Animal that you think lives longer.</p>
       <RandomAnimal />
       {/* <Popup /> */}
     </>
   );
 }
+
+
 
 export default App;
--- reanimal/src/components/RandomAnimal.js
+++ reanimal/src/components/RandomAnimal.js
@@ -1,12 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
-
+import useLocalStorage from "../hooks/useLocalStorage";
 const lifespans = [];
 export const names = [];
 
 export default function RandomAnimal() {
   const [animals, setAnimal] = useState([]);
   const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+  const [highScore, setHighScore] = useLocalStorage("highScore", 0);
 
   const checkGreaterTop = () => {
     if (lifespans[0] >= lifespans[1]) {
@@ -29,8 +29,8 @@ export default function RandomAnimal() {
       `You Win! ${names[0]}s live to ${lifespans[0]}, ${names[1]}s live to ${lifespans[1]}!`
     );
     setScore(score + 1);
-    if (score > highScore) {
-      setHighScore(score);
+    if (score + 1 > highScore) {
+      setHighScore(score + 1);
     }
     fetchAnimals();
   };
@@ -39,11 +39,7 @@ export default function RandomAnimal() {
     alert(
       `You Lose! ${names[0]}s live to ${lifespans[0]}, ${names[1]}s live to ${lifespans[1]}. Your score was ${score}!`
     );
-
     setScore(0);
-    if (score > highScore) {
-      setHighScore(score);
-    }
     fetchAnimals();
   };
 
@@ -64,66 +60,61 @@ export default function RandomAnimal() {
 
   if (animals) {
     return (
-      <div className="container-fluid">
-        <p>
-          Score: {score} High Score: {highScore}
-        </p>
+      <div className="container-fluid justify-content-center" id="AnimalInfo">
+        <h2 className="mx-auto text-center"> Score: {score} </h2>
+        <h4 className="mx-auto text-center"> High Score: {highScore}</h4>
+
         {animals.map((animal, id) => (
           <div key={id}>
             <div className="row m-2">
-              <div className="col-md-6">
-                <div className="card">
-                  <h5 className="card-header">{animal.name}</h5>
+              <div className="col-md-12">
+                <div className="card ">
+                  <h5
+                    className="card-header"
+                    onClick={id === 0 ? checkGreaterTop : checkGreaterBottom}
+                  >
+                    {animal.name}
+                  </h5>
                   <div className="card-body">
-                    <ul className="card-text">
-                      <li>Type: {animal.animal_type}</li>
-                      <li>Active Time: {animal.active_time}</li>
-                      <li>
-                        {" "}
-                        Length Range: {animal.length_min}-{animal.length_max}
-                        feet{" "}
-                      </li>
-                      <li>
-                        Weight Range: {animal.weight_min}-{animal.weight_max}
-                        lbs
-                      </li>
-                      {/* <li>Lifespan: {lifespan[id]} Years</li> */}
-                      <li>Habitat: {animal.habitat}</li>
-                      <li>Diet: {animal.diet}</li>
-                      <li>Geological Range: {animal.geo_range}</li>
-                    </ul>
+                    <div className="row">
+                      <div className="col-md-6" id="AnimalInfo">
+                        <ul className="card-text">
+    
+                          <li>Type: {animal.animal_type}</li>
+                          <li>Active Time: {animal.active_time}</li>
+                          <li>
+                            {" "}
+                            Length Range: {animal.length_min}-
+                            {animal.length_max}
+                            feet{" "}
+                          </li>
+                          <li>
+                            Weight Range: {animal.weight_min}-
+                            {animal.weight_max}
+                            lbs
+                          </li>
+                          {/* <li>Lifespan: {lifespan[id]} Years</li> */}
+                          <li>Habitat: {animal.habitat}</li>
+                          <li>Diet: {animal.diet}</li>
+                          <li>Geological Range: {animal.geo_range}</li>
+                        </ul>
+                      </div>
+          
+                      <div className="col-md-6" id="AnimalImage">
+                        <img
+                          alt={animal.name}
+                          src={animal.image_link}
+                          className="rounded mx-auto d-block"
+                        />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <img
-                  alt={animal.name}
-                  src={animal.image_link}
-                  className="rounded mx-auto"
-                />
               </div>
             </div>
           </div>
         ))}
-        <div className="row">
-          <div className="col-md-6">
-            <button
-              type="button"
-              className="btn btn-success btn-lg btn-block m-2"
-              onClick={checkGreaterTop}
-            >
-              Top
-            </button>
-
-            <button
-              type="button"
-              className="btn btn-success btn-lg btn-block m-2"
-              onClick={checkGreaterBottom}
-            >
-              Bottom
-            </button>
-          </div>
-        </div>
+        <div className="row"></div>
       </div>
     );
   }
